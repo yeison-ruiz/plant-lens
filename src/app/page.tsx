f84d2ecdb89804475aa01f535fc11f44845ef719
yeison ruiz <ingyeisonruiz26@gmail.com>
2024-11-21 17:25:31 -0500

    background degradado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import PlantIdentifier from "../components/PlantIdentifier";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-6 background-image">
-      <div className="absolute inset-0 gradient-overlay"></div>
+      <div className="absolute inset-0 bg-cover gradient-overlay"></div>
 
       <div className="relative z-10 w-full max-w-md">
         <PlantIdentifier />
